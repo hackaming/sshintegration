4416b1c4bfbbc66bd2df62c9519e03364bcfc905
hackaming <2251275876@qq.com>
2016-01-25 18:03:28 +0800

    add db create script
    add db create script

---
+++ springmvclearn/src/main/resources/Script.sql
@@ -0,0 +1,28 @@
+--<ScriptOptions statementTerminator=";"/>
+
+ALTER TABLE `springmvclearn`.`user` DROP PRIMARY KEY;
+
+ALTER TABLE `springmvclearn`.`project` DROP PRIMARY KEY;
+
+DROP TABLE `springmvclearn`.`project`;
+
+DROP TABLE `springmvclearn`.`user`;
+
+CREATE TABLE `springmvclearn`.`project` (
+	`id` INT NOT NULL,
+	`expectedAnnualRateOfReturn` FLOAT,
+	`holdingPeriod` TINYINT,
+	`purchaseAmount` INT,
+	`CrowdingProgress` FLOAT,
+	`crowdStatus` VARCHAR(64),
+	`projectName` VARCHAR(64),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `springmvclearn`.`user` (
+	`id` INT NOT NULL,
+	`userName` VARCHAR(64),
+	`password` VARCHAR(64),
+	PRIMARY KEY (`id`)
+);
+
